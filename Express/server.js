1d8b236761a831bf0f88d5da4f4cb7f4ed3fbece
gleyconxavier <gleyconxcarlos@gmail.com>
2020-02-13 18:29:42 -0400

    Calculadorinha em node

--- Express/server.js
+++ Express/server.js
@@ -1,8 +1,29 @@
-const express = require('express');
+// use esversion: 6
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("<h1>Testando</h1>");
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/home.html");
+});
+
+app.post("/", (req, res) => {
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+
+    const soma = num1 + num2;
+    const mult = num1 * num2;
+    const div = num1 / num2;
+    const sub = num1 - num2;
+
+
+    res.send("Resultado do calculo: <br>" +
+    "Soma: "+soma+"<br/>"+
+    "Multiplicação: "+mult+"<br/>"+
+    "Divisão: "+div+"<br/>"+
+    "Subtração: "+sub+"<br/>"
+    );
 });
 
 const port = 3000 || '' ;
