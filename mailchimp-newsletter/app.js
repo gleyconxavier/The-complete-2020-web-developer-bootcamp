73f79164381cd32c84048fa82745ba9f9269ba71
gleyconxavier <gleyconxcarlos@gmail.com>
2020-02-13 20:32:44 -0400

    Update | Newsletter front

--- mailchimp-newsletter/app.js
+++ mailchimp-newsletter/app.js
@@ -7,6 +7,10 @@ const port = 3000 || '';
 
 app.use(express.urlencoded());
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html');
+})
+
 app.listen(port, () => {
     console.log("Servidor esta rodando em: http://localhost:"+port);
 });
