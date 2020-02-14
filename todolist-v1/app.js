d255f7f9b01611ff6438a02f0555cc24e3438613
gleyconxavier <gleyconxcarlos@gmail.com>
2020-02-14 16:14:08 -0400

    Update | todo list ejs templating

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,47 +3,30 @@
 const express = require("express");
 const app = express();
 
+let items = ["Comprar comida", "Comprar software", "Comprar hardware"];
+
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     let today = new Date();
-    today = today.getDay();
-    let day = "";
-    if(today === 6 || today === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
 
-    switch (today) {
-        case 0: 
-            day = "Domingo";
-            break;
-        case 1:
-            day = "Segunda";
-            break;
-        case 2:
-            day = "Terça-feira";
-            break;
-        case 3:
-            day = "Quarta-feira";
-            break;
-        case 4:
-            day = "Quinta-feira";
-            break;
-        case 5:
-            day = "Sexta-feira";
-            break;
-        case 6:
-            day = "Sábado";
-            break;
-        default:
-        console.log("Error: current day is equal to: "+today);
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    res.render("list", { kindOfDay: day });
+    var day = today.toLocaleDateString("pt-BR", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", (req, res) => {
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 const port = 3000 || '';
